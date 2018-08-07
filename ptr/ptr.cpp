e93a432a41db92a53e65fa579bdcc1d58e9dcd1a
jiaweili <jiaweili005575@sohu-inc.com>
2018-08-07 17:34:01 +0800

    add shared_ptr

--- ptr/ptr.cpp
+++ ptr/ptr.cpp
@@ -1,12 +1,22 @@
 #include<iostream>
 #include<memory>
+#include<thread>
 using namespace std;
+void task(shared_ptr<int>a)
+{
+    shared_ptr<int>p(a);
+    cout<<"task thread:"<<p.use_count()<<endl;
+    cout<<"删除"<<endl;
+    //shared_ptr<int>p1(p,[]{delet})
+}
 int main()
 {
-    int *a=new int(10);
-    int *b=a;
-    cout<<a<<"  "<<b<<endl;
-    cout<<&a<<"  "<<&b<<endl;
-    shared_ptr<int>p;
+    shared_ptr<int>p(new int(10));
+    cout<<"main thread:"<<p.use_count()<<endl;
+    thread th(task,p);
+    cout<<"main thread:"<<p.use_count()<<endl;
+    th.join();
+    cout<<"main thread :"<<p.use_count()<<endl;
+    cout<<*p<<endl;
     return 0;
 }
